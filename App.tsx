821e1447e04a2401db1b1cf966162793e8a3c622
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-06-05 19:32:04 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,5 @@
+import {Text as RNText, TextProps} from 'react-native';
+
+export function Text({children, ...rest}: TextProps) {
+  return <RNText {...rest}>{children}</RNText>;
+}
