249fabfd836f37c2f1a0a646e8925cc54037cd49
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-07-27 07:41:11 -0300

    feat: useAppTheme

--- src/assets/icons/EyeOnIcon.tsx
+++ src/assets/icons/EyeOnIcon.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import {Svg, Path} from 'react-native-svg';
 
-// import {IconBase} from '../../components/Icon/Icon';
+import {IconBase} from '../../components/Icon/Icon';
 
-export function EyeOnIcon({size = 20, color = 'black'}) {
+export function EyeOnIcon({size = 20, color = 'black'}: IconBase) {
   return (
     <Svg width={size} height={size} viewBox="0 0 20 20" fill="none">
       <Path
